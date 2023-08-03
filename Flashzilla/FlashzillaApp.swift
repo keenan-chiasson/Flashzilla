0d6e8fcae27453b7d86673972cdc7569739419f4
keenan-chiasson <keenan.chiasson@acstexas.com>
2023-08-03 16:48:29 -0500

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Keenan Chiasson on 8/3/23.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

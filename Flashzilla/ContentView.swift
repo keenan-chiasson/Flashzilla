c3b5796951d4fa412e8fb69acebc05fc55a49e33
keenan-chiasson <keenan.chiasson@acstexas.com>
2023-08-06 21:55:05 -0500

    differentiateColor, reduceMotion, and reduceTransparency

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -9,20 +9,39 @@ import CoreHaptics
 import SwiftUI
 
 struct ContentView: View {
-    @Environment(\.scenePhase) var scenePhase
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    @Environment(\.accessibilityReduceTransparency) var reduceTransparency
+    @State private var scale = 1.0
     
     var body: some View {
-        Text("Hello, world")
-            .padding()
-            .onChange(of: scenePhase) { newPhase in
-                if newPhase == .active {
-                    print("Active")
-                } else if newPhase == .inactive {
-                    print("Inactive")
-                } else if newPhase == .background {
-                    print("Background")
-                }
+        HStack {
+            if differentiateWithoutColor {
+                Image(systemName: "checkmark.circle")
             }
+            
+            Text("Success")
+                .background(reduceTransparency ? .black : .black.opacity(0.5))
+                .foregroundColor(.white)
+                .clipShape(Capsule())
+        }
+        .padding()
+        .background(differentiateWithoutColor ? .black : .green)
+        .foregroundColor(.white)
+        .clipShape(Capsule())
+        .scaleEffect(scale)
+        .onTapGesture {
+            withOptionalAnimation {
+                scale *= 1.5
+            }
+        }
+    }
+    
+    func withOptionalAnimation<Result>(_ animation: Animation? = .default, body: () throws -> Result) rethrows -> Result {
+        if UIAccessibility.isReduceMotionEnabled {
+            return try body()
+        } else {
+            return try withAnimation(animation, body)
+        }
     }
 }
 

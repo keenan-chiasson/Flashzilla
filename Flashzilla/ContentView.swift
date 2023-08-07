fad2680a3f5d97317d9ab18085331e28eb2bc59c
keenan-chiasson <keenan.chiasson@acstexas.com>
2023-08-07 07:29:24 -0500

    reset to original state

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -5,43 +5,11 @@
 //  Created by Keenan Chiasson on 8/3/23.
 //
 
-import CoreHaptics
 import SwiftUI
 
 struct ContentView: View {
-    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
-    @Environment(\.accessibilityReduceTransparency) var reduceTransparency
-    @State private var scale = 1.0
-    
     var body: some View {
-        HStack {
-            if differentiateWithoutColor {
-                Image(systemName: "checkmark.circle")
-            }
-            
-            Text("Success")
-                .background(reduceTransparency ? .black : .black.opacity(0.5))
-                .foregroundColor(.white)
-                .clipShape(Capsule())
-        }
-        .padding()
-        .background(differentiateWithoutColor ? .black : .green)
-        .foregroundColor(.white)
-        .clipShape(Capsule())
-        .scaleEffect(scale)
-        .onTapGesture {
-            withOptionalAnimation {
-                scale *= 1.5
-            }
-        }
-    }
-    
-    func withOptionalAnimation<Result>(_ animation: Animation? = .default, body: () throws -> Result) rethrows -> Result {
-        if UIAccessibility.isReduceMotionEnabled {
-            return try body()
-        } else {
-            return try withAnimation(animation, body)
-        }
+        Text("Hello, world")
     }
 }
 
